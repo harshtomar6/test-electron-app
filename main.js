f766d64cc314db4a71caf205b819b175ff4d8a66
Harsh Tomar <harshtomar6@gmail.com>
2017-09-02 00:42:29 +0530

    Initial and probably final

---
+++ main.js
@@ -0,0 +1,10 @@
+let electron = require('electron')
+let {app, BrowserWindow } = electron
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 1300, height: 700})
+    mainWindow.loadURL("file://"+__dirname+'/src/index.html')
+    //mainWindow.webContents.openDevTools()
+})
